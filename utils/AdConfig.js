69803d2f301f606c7ebd95e47cd486368f740ea7
kayteedberserker <kayteedberserker@gmail.com>
2026-01-14 11:43:59 -0800

    Update AdConfig with production ad IDs
    Updated AdConfig with production ad IDs and fixed formatting.

--- utils/AdConfig.js
+++ utils/AdConfig.js
@@ -1,18 +1,22 @@
 import { TestIds } from 'react-native-google-mobile-ads';
 
 // Set this to true when you are ready to go live
-const IS_PRODUCTION = !__DEV__; 
+const IS_PRODUCTION =! __DEV__; 
 
 export const AdConfig = {
-  banner: IS_PRODUCTION 
-    ? 'ca-app-pub-xxxxxxxxxxxxxxxx/yyyyyyyyyy' // Real Banner ID
+  banner: IS_PRODUCTION
+    ? 'ca-app-pub-7937601457401494/9433498196'
     : TestIds.BANNER,
-    
-  interstitial: IS_PRODUCTION 
-    ? 'ca-app-pub-xxxxxxxxxxxxxxxx/zzzzzzzzzz' // Real Interstitial ID
+
+  interstitial: IS_PRODUCTION
+    ? 'ca-app-pub-7937601457401494/8875272604'
     : TestIds.INTERSTITIAL,
 
   rewarded: IS_PRODUCTION
-    ? 'ca-app-pub-xxxxxxxxxxxxxxxx/wwwwwwwwww' // Real Rewarded ID
+    ? 'ca-app-pub-7937601457401494/9241926507'
     : TestIds.REWARDED,
+
+  appOpen: IS_PRODUCTION
+    ? 'ca-app-pub-7937601457401494/1256637033' // App Open ID
+    : TestIds.APP_OPEN,
 };
