1f8b5653dc1c64b80b98c0c0014006e76dd33b31
kayteedberserker <kayteedberserker@gmail.com>
2026-01-15 15:28:05 +0100

    Update AdConfig.js

--- utils/AdConfig.js
+++ utils/AdConfig.js
@@ -1,7 +1,7 @@
 import { TestIds } from 'react-native-google-mobile-ads';
 
 // Set this to true when you are ready to go live
-const IS_PRODUCTION = false
+const IS_PRODUCTION = true
 
 export const AdConfig = {
   banner: IS_PRODUCTION
