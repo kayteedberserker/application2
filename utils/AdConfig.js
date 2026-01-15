065a022dbb9b4bf406dba27cad541733e175e4ed
kayteedberserker <kayteedberserker@gmail.com>
2026-01-15 10:34:22 +0100

    Update AdConfig.js

--- utils/AdConfig.js
+++ utils/AdConfig.js
@@ -1,22 +1,22 @@
 import { TestIds } from 'react-native-google-mobile-ads';
 
 // Set this to true when you are ready to go live
-const IS_PRODUCTION =! __DEV__; 
+const IS_PRODUCTION = !__DEV__; 
 
 export const AdConfig = {
   banner: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/9433498196'
+    ? 'ca-app-pub-7937601457401494/3239678831'
     : TestIds.BANNER,
 
   interstitial: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/8875272604'
+    ? 'ca-app-pub-7937601457401494/9421943807'
     : TestIds.INTERSTITIAL,
 
   rewarded: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/9241926507'
+    ? 'ca-app-pub-7937601457401494/2696753353'
     : TestIds.REWARDED,
 
   appOpen: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/1256637033' // App Open ID
+    ? 'ca-app-pub-7937601457401494/3563112460' // App Open ID
     : TestIds.APP_OPEN,
 };
