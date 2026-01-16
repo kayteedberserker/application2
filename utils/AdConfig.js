1881df98bf251b456492b32c91063b023973b7ae
kayteedberserker <kayteedberserker@gmail.com>
2026-01-16 21:20:30 +0100

    Update AdConfig.js

--- utils/AdConfig.js
+++ utils/AdConfig.js
@@ -5,18 +5,19 @@ const IS_PRODUCTION = true
 
 export const AdConfig = {
   banner: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/3239678831'
+    ? 'ca-app-pub-8021671365048667/7318309040'
     : TestIds.BANNER,
 
   interstitial: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/9421943807'
+    ? 'ca-app-pub-8021671365048667/6872598240'
     : TestIds.INTERSTITIAL,
 
-  rewarded: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/2696753353'
-    : TestIds.REWARDED,
+  rewarded: TestIds.REWARDED, 
+    //IS_PRODUCTION
+ //   ? 'ca-app-pub-8021671365048667/5559516575'
+  //  : TestIds.REWARDED,
 
   appOpen: IS_PRODUCTION
-    ? 'ca-app-pub-7937601457401494/3563112460' // App Open ID
+    ? 'ca-app-pub-8021671365048667/8591846359' // App Open ID
     : TestIds.APP_OPEN,
 };
